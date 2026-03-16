7e28c59c32d89ec3bc65b166172fa7965be474be
Sidney2022 <uwayasidney@gmail.com>
2026-03-16 14:09:26 +0100

    requirements

--- chowbuddies/wsgi.py
+++ chowbuddies/wsgi.py
@@ -1,16 +1,30 @@
-"""
-WSGI config for chowbuddies project.
+# """
+# WSGI config for chowbuddies project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+# """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+import sys
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chowbuddies.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+
+
+
+def application(environ, start_response):
+    status = '200 OK'
+    output = []
+    output.append(f"Python executable: {sys.executable}\n")
+    output.append(f"sys.path:\n" + "\n".join(sys.path) + "\n")
+    output.append("site-packages should be somewhere above\n")
+
+    response_headers = [('Content-type', 'text/plain'),
+                        ('Content-Length', str(len(''.join(output))))]
+    start_response(status, response_headers)
+    return [line.encode('utf-8') for line in output]
